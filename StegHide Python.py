d8609727684d1f478cfab84c93b8bf31f12efc45
Anshul Chaturvedi <64137392+h1ghj4ck3r@users.noreply.github.com>
2023-11-26 15:15:21 +0530

    StegHide Python.py

--- StegHide Python.py
+++ StegHide Python.py
@@ -1,11 +1,12 @@
 import cv2
 import os
-import string
 
+# Read the image
+img = cv2.imread("target.jpg")
 
-img = cv2.imread("mypic.jpg") # Replace with the correct image path
-msg = input("Enter secret message:")
-password= input("Enter a passcode:")
+msg = input("Enter secret message:  ")
+password = input("Enter a passcode:  ")
+
 d = {}
 c = {}
 
@@ -24,20 +25,23 @@ for i in range(len(msg)):
     z = (z + 1) % 3
 
 cv2.imwrite("encryptedImage.jpg", img)
-os.system("start encryptedImage.jpg") # Use 'start' to open the image on Windows
+
+os.system("start encryptedImage.jpg")
 
 message = ""
 n = 0
 m = 0
 z = 0
 
-pas = input("Enter passcode for Decryption")
+
+pas = input("Enter passcode for Decryption:  ")
+
 if password == pas:
     for i in range(len(msg)):
         message = message + c[img[n, m, z]]
         n = n + 1
         m = m + 1
-        z = (z+1)%3
-        print("Decryption message:", message)
+        z = (z + 1) % 3
+    print("Decryption message:", message)
 else:
     print("YOU ARE NOT auth")
